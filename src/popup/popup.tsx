42335fc2b2629f9beb9069fdd3a41e9213b44cb3
IhorHudkov <gudkov.igor@gmail.com>
2022-07-27 16:58:28 +0300

    Add Weather API util

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './popup.css'
+import React, { useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+import './popup.css';
+import { fetchOpenWeatherData } from '../utils/api';
 
 const App: React.FC<{}> = () => {
+  useEffect(() => {
+    fetchOpenWeatherData('Toronto')
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
       <img src="icon.png" />
     </div>
-  )
-}
+  );
+};
 
-const root = document.createElement('div')
-document.body.appendChild(root)
-ReactDOM.render(<App />, root)
+const container = document.createElement('div');
+document.body.appendChild(container);
+const root = createRoot(container!);
+root.render(<App />);
